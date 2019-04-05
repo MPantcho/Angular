f6c535ee36311451fa22576efcc0e6a6f14ff0d7
Mathieu PANTCHENKO <mathieu.pantchenko@edu.univ-fcomte.fr>
2019-04-05 11:32:05 +0200

    cardOkLocal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApiModule } from './rest';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MComponent } from './m/m.component';
+import { ListComponent } from './product/list/list.component';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApiModule,
+    HttpClientModule,
+    ProductModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
